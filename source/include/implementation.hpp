abcb72124518ce1ec0f4fb95d0b736cf9f77325c
Ariel Bernal <ariel.j.bernal@intel.com>
2018-08-15 17:18:26 -0700

    test

---
+++ source/include/implementation.hpp
@@ -0,0 +1,129 @@
+#pragma once
+
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <functional>
+#include <memory>
+#include <utilities.hpp>
+
+namespace syDNN {
+
+const size_t SY_MAX_KERNELS = 5;
+
+class Kernel {
+public:
+  Kernel(const cl::Context& context, const std::string& sourceFilename, const std::string& name)
+  : _context(context)
+  , _name(name)
+  , _source(load_program(sourceFilename))
+  , _global_work_size(cl::NullRange)
+  , _global_work_offset(cl::NullRange)
+  , _local_work_size(cl::NullRange)
+  {}
+
+  cl::Kernel operator()() { return _kernel; }
+  cl_int enqueue(cl::CommandQueue& queue, const std::vector<cl::Event>* events = nullptr, cl::Event* event = nullptr) {
+    return queue.enqueueNDRangeKernel(_kernel, _global_work_offset, _global_work_size, _local_work_size, events, event);
+  }
+  void compile(const std::string& preamble = "", const std::string& options = "") {
+    _kernel = syDNN::compile_kernel(_context, preamble + _source, _name, options);
+  }
+
+  template<typename T>
+  void add_argument(const T arg) { _kernel.setArg(_idx++, arg); }
+
+  void global_work_size(const cl::NDRange& range) { _global_work_size = range; }
+  void global_work_offset(const cl::NDRange& range) { _global_work_offset = range; }
+  void local_work_size(const cl::NDRange& range) { _local_work_size = range; }
+private:
+  size_t _idx = 0;
+  cl::Context _context;
+  std::string _name;
+  std::string _source;
+  cl::Kernel _kernel;
+  cl::NDRange _global_work_size;
+  cl::NDRange _local_work_size;
+  cl::NDRange _global_work_offset;
+};
+
+class Implementation {
+public:
+  Implementation() = default;
+  Implementation(const cl::Context context) : _context(context) {}
+  Kernel& kernel(size_t kernel_id = 0) { return _kernels[kernel_id]; }
+  void add_kernel(const std::string& name, const std::string& sourceFilename) { _kernels.emplace_back(_context, name, sourceFilename); }
+  cl::Context context() { return _context; }
+
+  virtual void compile() = 0;
+  virtual void set_arguments() = 0;
+  virtual cl_int enqueue(cl::CommandQueue queue, const std::vector<cl::Event>* events = nullptr, cl::Event* event = nullptr) {
+    Kernel& k = kernel();
+    return k.enqueue(queue, events, event);
+  }
+private:
+  cl::Context _context;
+  std::vector<Kernel> _kernels;
+};
+
+
+template<class T, typename ConstructorFunc>
+struct RegistryElement {
+  std::string name;
+  bool default_implementation = false;
+  ConstructorFunc f;
+  std::shared_ptr<T> object;
+};
+
+template<class T, typename ConstructorFunc>
+class FactoryBase {
+public:
+  using RegistryMap = std::unordered_map<std::string, RegistryElement<T, ConstructorFunc>>;
+
+  static const RegistryMap& implementations() { return registry(); }
+  static const std::string& default_implementation() { return default_name(); }
+
+  template<typename... Args>
+  static std::unique_ptr<T> create(const std::string& name, Args&&... args) {
+    auto it = registry().find(name != "" ? name : default_name());
+    if (it == registry().end())
+      throw std::runtime_error("ImplementationFactory::create");
+    return it->second.f(std::forward<Args>(args)...);
+  }
+protected:
+  template<typename TO>
+  static bool register_impl(const std::string& name, bool default_impl, ConstructorFunc constructorFunc) {
+    auto it = registry().find(name);
+    if (it == registry().end()) {
+      registry()[name] = { name, default_impl, constructorFunc, std::shared_ptr<T>() };
+      if (default_impl)
+        default_name() = name;
+      return true;
+    }
+    return false;
+  }
+  static std::shared_ptr<T> get_instance(const std::string& name) {
+    auto it = registry().find(name != "" ? name : default_name());
+    if (it == registry().end())
+      throw std::runtime_error("ImplementationFactory::get_instance");
+    return it->second.object;
+  }
+private:
+  FactoryBase() {};
+  FactoryBase(FactoryBase const&) = delete;
+  void operator=(FactoryBase const&) = delete;
+
+  static std::string& default_name() {
+    static std::string impl;
+    return impl;
+  }
+
+  static RegistryMap& registry() {
+    static RegistryMap impl;
+    return impl;
+  }
+};
+
+
+} // namespace syDNN
---
+++ source/include/size.hpp
@@ -0,0 +1,226 @@
+#pragma once
+
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <utilities.hpp>
+
+namespace syDNN
+{
+
+using size_value_type = int32_t;
+const size_t MAX_SIZE_DIM = 16;
+
+class Size {
+public:
+  using Array = std::array<size_value_type, MAX_SIZE_DIM>;
+
+  Size() {}
+
+  Size(const Size& rhs)
+  : _arr(rhs._arr)
+  , _size(rhs._size)
+  {}
+
+  Size(const size_value_type* c_arr, size_t n)
+  : _size(n)
+  {
+    std::copy(c_arr, c_arr + _size, std::begin(_arr));
+  }
+
+  template<typename T, class = typename std::enable_if<std::is_same<T, std::vector<size_t>>::value>::type>
+  Size(const T& arr)
+  : _size(arr.size())
+  {
+    std::copy(&arr[0], &arr[0] + _size, std::begin(_arr));
+  }
+
+  Size(const std::initializer_list<int32_t>& il)
+  : _size(il.size())
+  {
+    std::copy(il.begin(), il.end(), std::begin(_arr));
+  }
+
+  template<typename... Ts, class = typename std::enable_if<all_integral<Ts...>::value>::type>
+  Size(Ts&&... ts)
+  : _arr({std::forward<size_value_type>(ts)...})
+  , _size(sizeof...(Ts))
+  {
+    static_assert(all_same<size_value_type, Ts...>::value, "Size elements should be of integral type");
+    static_assert(sizeof...(Ts) <= MAX_SIZE_DIM, "Number of dimensions greater than MAX_SIZE_DIM");
+  }
+
+  void resize(size_t n, size_value_type v = 0) {
+    if (n > MAX_SIZE_DIM)
+      throw std::out_of_range("Size::resize");
+    _size = n;
+    _arr.fill(v);
+  }
+
+  void fill(size_value_type v = 0) {
+    _arr.fill(v);
+  }
+
+  void range(size_value_type n, size_value_type step) {
+    for (size_t i = 0; i < _size; ++i)
+      _arr[i] = n + step * i;
+  }
+
+  static Size Fill(size_t n, size_value_type v = 0) {
+    Size ret;
+    ret.resize(n, v);
+    return ret;
+  }
+
+  static Size Zeros(size_t n) {
+    Size ret;
+    ret.resize(n, 0);
+    return ret;
+  }
+
+  static Size Range(size_value_type n0, size_value_type n1, size_value_type step = size_value_type(1)) {
+    Size ret;
+    if (n1 < n0 && step > 0 || n0 < n1 && step < 0 || step == 0)
+      return ret;
+    size_t n = std::ceil(std::fabs(n1 - n0) / std::fabs(step));
+    ret.resize(n, 0);
+    ret.range(n0, step);
+    return ret;
+  }
+
+  static Size Range(size_value_type n) {
+    return Range(0, n);
+  }
+
+  // TODO: sub_size to be tested and add exceptions
+  Size sub_size(size_t start, size_t end) const {
+    std::vector<size_t> v;
+    for (size_t i = start; i < end; ++i) v.push_back(_arr[i]);
+    return Size(v);
+  }
+
+  Size sub_left(size_t end) const {
+    std::vector<size_t> v;
+    for (size_t i = 0; i < end; ++i) v.push_back(_arr[i]);
+    return Size(v);
+  }
+
+  Size sub_right(size_t start) const {
+    std::vector<size_t> v;
+    for (size_t i = start; i < _size; ++i) v.push_back(_arr[i]);
+    return Size(v);
+  }
+
+  size_value_type& operator[](size_t n) {
+    return _arr[n];
+  }
+
+  size_value_type operator[](size_t n) const {
+    return _arr[n];
+  }
+
+  const size_value_type& at(size_t n) const {
+    if (n > _size)
+      throw std::out_of_range("Size::at");
+    return _arr[n];
+  }
+
+  Size& operator=(const Size& rhs) {
+    _arr = rhs.data();
+    _size = rhs.size();
+    return *this;
+  }
+
+  void operator=(const size_value_type rhs) {
+    _arr.fill(rhs);
+  }
+
+  Size add(const Size& rhs) const {
+    if (rhs._size != _size)
+      throw std::runtime_error("Size::add");
+    Size s = *this;
+    for (int i = 0; i < _size; ++i)  s[i] += rhs[i];
+    return s;
+  }
+
+  Size mul(const size_value_type rhs) const {
+    Size s = *this;
+    for (int i = 0; i < _size; ++i)  s[i] *= rhs;
+    return s;
+  }
+
+  Size& operator+=(const Size& rhs) {
+    if (rhs._size != _size)
+      throw std::runtime_error("Size::+=");
+    for (size_t i = 0; i < _size; i++)
+      _arr[i] += rhs[i];
+    return *this;
+  }
+
+  Size& operator*=(const size_value_type rhs) {
+    for (size_t i = 0; i < _size; i++)
+      _arr[i] *= rhs;
+    return *this;
+  }
+
+  bool operator==(const Size& rhs) const {
+    if (rhs._size != _size)
+      return false;
+    return std::equal(_arr.begin(), _arr.begin() + _size, rhs.data().begin());
+  }
+
+  size_value_type dim() const {
+    return std::accumulate(_arr.begin(),_arr.begin() + _size, static_cast<size_value_type>(1), std::multiplies<size_t>());
+  }
+
+  size_t size() const {
+    return _size;
+  }
+
+  const Array& data() const {
+    return _arr;
+  }
+
+  Array data() {
+    return _arr;
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, const Size& s) {
+    os << s.to_string();
+    return os;
+  }
+
+  std::string to_string() const {
+    std::stringstream ret;
+    ret << "[";
+    if (_size > 0) {
+      for (size_t i = 0; i < _size - 1; ++i)
+        ret << _arr[i] << ", ";
+      ret << _arr[_size - 1];
+    }
+    ret << "]";
+    return ret.str();
+  }
+
+private:
+  Array _arr;
+  size_t _size = 0;
+};
+
+inline Size operator+(const Size& lhs, const Size& rhs) { return lhs.add(rhs); }
+inline Size operator*(const Size& lhs, const size_value_type rhs) { return lhs.mul(rhs); }
+inline Size operator*(const size_value_type lhs, const Size& rhs) { return rhs.mul(lhs); }
+
+inline Size align(const Size& v, const Size& alignment) {
+  if (v.size() != alignment.size())
+    throw std::runtime_error("align(Size&, Size&)");
+  Size s = Size::Zeros(v.size());
+  for (size_t i = 0; i < s.size(); ++i)
+    s[i] = syDNN::align(v[i], alignment[i]);
+  return s;
+}
+
+} // namespace syDNN
